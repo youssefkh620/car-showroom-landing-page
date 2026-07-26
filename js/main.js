32987f0086cbb9069ae675393c342c3f0b41d39c
Youssef Khaled <yousefkh620@gmail.com>
2026-07-26 14:48:28 +0300

    Add hero section

--- js/main.js
+++ js/main.js
@@ -5,3 +5,18 @@ const menuDiv = document.getElementById("menuDiv");
 menuBtn.addEventListener("click", function () {
   menu.classList.toggle("hidden");
 });
+
+const navBg = document.getElementById("navBg");
+const navList = document.getElementById("navList");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 20) {
+    navBg.classList.add("bg-white", "shadow-sm");
+    navList.classList.remove("text-white");
+    navList.classList.add("text-[#364153]");
+  } else {
+    navBg.classList.remove("bg-white", "shadow-sm");
+    navList.classList.remove("text-[#364153]");
+    navList.classList.add("text-white");
+  }
+});
