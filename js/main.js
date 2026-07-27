626a0c28b880ad65631b527540cd493309a28e54
Youssef Khaled <yousefkh620@gmail.com>
2026-07-27 07:05:09 +0300

    Add dynamic featured cars section using JSON data

--- js/main.js
+++ js/main.js
@@ -20,3 +20,82 @@ window.addEventListener("scroll", function () {
     navList.classList.add("text-white");
   }
 });
+
+const carsContainer = document.getElementById("carsContainer");
+
+fetch("car.json")
+  .then((response) => response.json())
+  .then((cars) => {
+    cars.forEach((car) => {
+      carsContainer.innerHTML += `
+
+    <div
+      class="rounded-xl bg-white shadow-lg border border-gray-100 overflow-hidden"
+    >
+
+      <img 
+      class="w-full" 
+      src="${car.image}" 
+      alt="car" />
+
+      <div class="px-6 py-2">
+
+        <div class="flex items-end justify-between mb-4">
+
+          <div>
+            <h4 class="font-primary font-bold text-[10px] text-[#99A1AF]">
+              ${car.brand}
+            </h4>
+
+            <p class="font-primary font-bold text-[#101828]">
+              ${car.model}
+            </p>
+          </div>
+
+
+          <p class="font-primary font-black text-[#E7000B]">
+            ${car.price}
+          </p>
+
+        </div>
+
+
+        <span class="block m-auto h-0.5 bg-[#F3F4F6] w-full my-3.5"></span>
+
+
+        <div
+          class="flex items-center gap-4 text-[11px] text-[#6A7282] mb-3.5"
+        >
+
+          <p>
+          <i class="fa-solid fa-gas-pump"></i> 
+          ${car.type}
+          </p>
+
+          <p>
+          <i class="fa-solid fa-car"></i> 
+          ${car.transmission}
+          </p>
+
+          <p>
+          <i class="fa-solid fa-bolt"></i> 
+          ${car.power}
+          </p>
+
+        </div>
+
+
+        <button
+          class="font-primary font-bold text-xs text-[#364153] border-2 border-[#E5E7EB] w-full rounded-3xl py-3 hover:cursor-pointer"
+        >
+          View Details
+        </button>
+
+
+      </div>
+
+    </div>
+
+    `;
+    });
+  });
